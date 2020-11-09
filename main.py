b8118d0a616cb2e08ca48d6f8831ecd4f16fda6a
Emmanuel GUENOU <emmanuel@compuute.io>
2020-11-09 12:52:20 +0000

    birthdate functionalities set

---
+++ main.py
@@ -0,0 +1,15 @@
+from numerology import PythagoreanNumerology
+
+
+def start_app():
+    """Interactive version of the package."""
+    print("Starting...\n")
+    
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    birthdate = input("Enter your birthdate (yyyy-MM-dd, example 1994-11-30): ")
+    
+    my_numerology = PythagoreanNumerology(first_name, last_name, birthdate)
+
+if __name__ == "__main__":
+    start_app()
