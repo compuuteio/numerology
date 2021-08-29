daaf8ff6e6d6bf20639094fdd2b4d915f2ccfe7b
David Wright <david_v_wright@yahoo.com>
2021-08-29 12:19:40 -0700

    fix imports, paths

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from numerology import PythagoreanNumerology
+from numerology import Pythagorean
 
 
 def start_app():
     """Interactive version of the package."""
     print("Starting...\n")
-    
+
     first_name = input("Enter your first name: ")
     last_name = input("Enter your last name: ")
     birthdate = input("Enter your birthdate (yyyy-MM-dd, example 1994-11-30): ")
-    
-    my_pythagorean_numerology = PythagoreanNumerology(first_name, last_name, birthdate)
+
+    Pythagorean(first_name, last_name, birthdate)
+
 
 if __name__ == "__main__":
     start_app()
--- numerology/main.py
+++ numerology/main.py
@@ -1,6 +1,9 @@
 import gettext
 import locale
+import os
+import sys
 
+localedir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "locale")
 
 default_lang = "en"
 try:
@@ -10,11 +13,15 @@ except:
     # If unable to get the locale language, use English
     lang = default_lang
 try:
-    language = gettext.translation('numerology', localedir='locale', languages=[lang])
+    language = gettext.translation(
+        "numerology", localedir=localedir_path, languages=[lang]
+    )
 except:
     # If the current language does not have a translation, the default laguage (English) will be used English
-    language = gettext.translation('numerology', localedir='locale', languages=[default_lang])
+    language = gettext.translation(
+        "numerology", localedir=localedir_path, languages=[default_lang]
+    )
 language.install()
 _ = language.gettext
-    
+
 print(_("Welcome!"))
